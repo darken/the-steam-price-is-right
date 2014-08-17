6e8094d1def0d06e5eab965fd99aaf886a34b6a6
Gustavo Rodríguez <darken.dev@gmail.com>
2014-08-16 21:20:13 -0500

    Removed play dependency, say.js file added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var createChecker = require('./lib/checker').createChecker,
-    readArgs = require('./lib/read-args');
+    readArgs      = require('./lib/read-args'),
+    paramsArr     = readArgs('items_to_check');
 
-var prevChecker;
-var paramsArr = readArgs('items_to_check')
 if (!paramsArr.length) throw 'items_to_check file can\'t be empty';
 
+var prevChecker;
 paramsArr.forEach(function (params) {
   var checker = createChecker.apply(null, params);
   if (prevChecker) prevChecker.chain(checker);
